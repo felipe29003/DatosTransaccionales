1271aa9286b3ca993c7bd88780c0457f78627255
Juan David Otero <104285928+Juan-Otero@users.noreply.github.com>
2023-11-07 18:08:29 -0500

    Carpeta de Documentación

---
+++ Documentación/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"SocketsChat","l":"Cliente"},{"p":"SocketsChat","l":"ManejadorCliente"},{"p":"SocketsChat","l":"Servidor"}];updateSearchResults();
